375dc421ca518a9dc55bac24cc30158bfb2ee82c
Jürgen Kartnaller <juergen@kartnaller.at>
2012-12-23 09:36:09 +0100

    version bump

--- pyes/__init__.py
+++ pyes/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-VERSION = (0, 19, 2, "ls1")
+VERSION = (0, 19, 2, "ls2")
 
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Alberto Paro"
